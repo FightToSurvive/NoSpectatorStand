6e0509ac53f0fd02602024ae5512de64e280e77f
2stinkysocks <zmehall@gmail.com>
2024-01-23 00:43:33 -0700

    Add a toggle keybind

--- src/main/java/me/twostinkysocks/nospectatorstand/client/mixin/ArmorStandArmorEntityModelMixin.java
+++ src/main/java/me/twostinkysocks/nospectatorstand/client/mixin/ArmorStandArmorEntityModelMixin.java
@@ -1,5 +1,6 @@
 package me.twostinkysocks.nospectatorstand.client.mixin;
 
+import me.twostinkysocks.nospectatorstand.client.NoSpectatorStandClient;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.ArmorStandArmorEntityModel;
@@ -7,7 +8,6 @@ import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ArmorStandArmorEntityModel.class)
 public abstract class ArmorStandArmorEntityModelMixin extends BipedEntityModel<ArmorStandEntity> {
@@ -16,10 +16,9 @@ public abstract class ArmorStandArmorEntityModelMixin extends BipedEntityModel<A
         super(modelPart);
     }
 
-
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
-        if(alpha == 1.0) {
+        if(!NoSpectatorStandClient.enabled || alpha == 1.0) {
             super.render(matrices, vertices, light, overlay, red, green, blue, alpha);
         }
     }
